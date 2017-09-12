cc4bf3d4a408a823b48179f7b638bbf98710c24d
raku1414 <mokosu@vivaldi.net>
2017-09-12 20:53:21 +0900

    did

---
+++ wacomset.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+xsetwacom --set 10 Area 0 0 15200 8550
+xsetwacom --set 12 Area 0 0 15200 8550
+xsetwacom --set 10 MapToOutput "1920x1080+0+0"
+xsetwacom --set 12 MapToOutput "1920x1080+0+0"
